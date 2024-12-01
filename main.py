78da03759247d81a1d07c145a4d370577be4cec4
MASRITA MANGALARAPU <90557850+MASRITA19@users.noreply.github.com>
2024-12-01 20:47:51 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+# main.py
+def add(a, b):
+    """Add two numbers."""
+    return a + b
+
+def subtract(a, b):
+    """Subtract two numbers."""
+    return a - b
+
+if __name__ == "__main__":
+    print(f"Addition: {add(10, 5)}")
+    print(f"Subtraction: {subtract(10, 5)}")
